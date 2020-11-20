72f2c1adac26fb3bdc2a78721769cb7248e12916
Viktor Varland <viktor@vardevs.se>
2020-11-20 13:13:05 +0100

    feat: comment and close action

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+    const exempt_users = core.getInput('exempt-users')    
+    const close_comment = core.getInput('close-comment')
+    const gh_token = core.getInput('github-token')
+
+    core.setOutput('Close with comment', close_comment)
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+} catch (error) {
+    core.setFailed(error.message)
+}
