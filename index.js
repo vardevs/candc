c57e44f34d944494272ca67c74eb44446e9b8333
Viktor Varland <viktor@vardevs.se>
2020-11-20 13:18:45 +0100

    fix: sequences not allowed

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ try {
 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-
 } catch (error) {
     core.setFailed(error.message)
 }
