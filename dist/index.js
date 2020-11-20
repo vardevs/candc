28aa6ce28ad5b7bdbf841bcce2b67c8939e43416
Viktor Varland <viktor@vardevs.se>
2020-11-20 13:48:26 +0100

    fix: pass in context.repo

--- dist/index.js
+++ dist/index.js
@@ -15,17 +15,14 @@ try {
 }
 
 async function main () {
-    const exempt_users = core.getInput('exempt-users').split(',')
+    const exempt_users = core.getInput('exempt-users')
     const close_comment = core.getInput('close-comment')
     const gh_token = core.getInput('github-token')
 
     const number = github.context.payload.issue.number
     const user = github.context.payload.issue.user
 
-    if (exempt_users.includes(user)) {
-        core.info(`${user} is exempt, leaving issue open.`)
-        return
-    }
+    console.log(exempt_users)
 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`)
@@ -33,21 +30,22 @@ async function main () {
     const octokit = github.getOctokit(gh_token)
     const context = github.context
 
+    console.log(context.repo)
     try {
         await octokit.issues.createComment({
-            ...context,
+            ...context.repo,
             body: close_comment,
         })
 
         await octokit.issues.edit({
-            ...context,
+            ...context.repo,
             state: 'closed'
         })
+
+        core.info(`Closed issue ${number} from ${user}.`)
     } catch (error) {
         core.setFailed(error.message)
     }
-
-    core.info(`Closed issue ${number} from ${user}.`)
 }
 
 
